3a9e07edd63dd3ea9d5227382527e18b61819af5
zkmartin <653938079@qq.com>
2018-07-03 09:26:40 +0800

    modified paraxiugaimeters

--- at/multiinput_task.py
+++ at/multiinput_task.py
@@ -6,14 +6,12 @@ from tframe.utils.misc import mark_str as ms
 import model_lib as models
 import os
 
-# os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-# os.environ["CUDA_VISIBLE_DEVICES"] = core.th.visible_gpu_id
 def main(_):
   console.start('Multinput task')
 
   # Configurations
   th = core.th
-  th.job_dir = 'debug_test'
+  th.job_dir = 'rand_pos'
   th.model = models.multiinput
   th.actype1 = 'relu'
 
@@ -26,11 +24,11 @@ def main(_):
   th.patience = 100
   th.shuffle = True
 
-  # th.train = False
+  th.train = False
   th.smart_train = False
   th.max_bad_apples = 4
   th.lr_decay = 0.6
-  th.rand_over_classes = True
+  th.rand_over_classes = False
 
   th.save_model = True
   th.overwrite = True
@@ -45,20 +43,20 @@ def main(_):
   th.mfcc_keep_prob = 0.7
   th.concat_keep_prob = 0.9
   th.fold = 0
-  th.shuffle = False
+  # th.shuffle = False
   
   th.rand_pos = True
+  th.test_all = False
   
-  # fold = Flag.integer(0, 'cross-validation fold id', is_key=True)
-  # fold.register('fold')
-  # description = 'cnn_raw_data_mfcc_random_rand'
+  th.visible_gpu_id = '0'
+  
   # description = 'raw_data_mfcc_dropout_{}_random_{}_fold_{}'.format(
   #               th.mfcc_keep_prob, th.concat_keep_prob, th.fold)
-  # description = 'recover_test'
-  description = 'raw_data_mfcc_simplified_dropout_0.7_0.9_{}'.format(th.fold)
+  description = 'raw_data_mfcc_dropout_{}_{}'.format(th.mfcc_keep_prob,
+                                                     th.concat_keep_prob)
+  # description = 'raw_data_mfcc_simplified_dropout_0.7_0.9_{}'.format(th.fold)
   # description = 'raw_data_mfcc_simlified_dropout_0.7_reg_0.2_sap_all'
   th.mark = 'cnn_{}'.format(description)
-  if th.rand_pos:th.mark += '_rand_pos'
 
   export_false = True
   core.activate()
